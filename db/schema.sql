b0d0d1cd6084605b63277ca4088cc6293cd22b0c
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-10-07 18:17:03 -0500

    seed data

--- db/schema.sql
+++ db/schema.sql
@@ -7,21 +7,24 @@ USE company_db;
 
 -- tables 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
-    name VARCHAR(30)
+    id INT NOT NULL AUTO_INCREMENT,
+    dept_name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-    id INT PRIMARY KEY,
+CREATE TABLE emp_role (
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT
+    department_id INT,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT
+    manager_id INT,
+    PRIMARY KEY (id)
 );
