0898cc2801a20438194bfcf82c8648a8bdf9cda8
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-10-07 20:42:27 -0500

    foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,8 @@ CREATE TABLE emp_role (
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -26,5 +27,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES emp_role(id)
+    -- FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
