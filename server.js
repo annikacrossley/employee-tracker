8b20b937f67751c9d07297f058bced7ecc0b5a37
annikacrossley <132411887+annikacrossley@users.noreply.github.com>
2023-10-07 20:27:24 -0500

    async await

--- server.js
+++ server.js
@@ -7,4 +7,18 @@ const connection = mysql.createConnection({
     database: 'company_db'
 });
 
-console.log(connection)
+const init = async () => {
+    
+    const results = await connection.promise().query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+        ('annika', 'crossley', 1, 4)`,
+    )
+
+    const company = await connection.promise().query(
+        'SELECT * FROM employee'
+    )
+
+    console.log(company)
+}
+
+init()
